3eb5db369f391ad3383be01bfdcb2bfb47d16708
gedithejedi <33606955+gedithejedi@users.noreply.github.com>
2024-11-17 07:09:36 +0700

    feat: merge changes

--- web/app/explore/[chain]/[address]/page.tsx
+++ web/app/explore/[chain]/[address]/page.tsx
@@ -1,27 +1,27 @@
-'use client';
+"use client";
 
-import { useQuery } from '@tanstack/react-query';
-import toast from 'react-hot-toast';
-import axios from 'axios';
-import { usePathname } from 'next/navigation';
-import { baseUrl } from '@/utils/constants';
-import formatFilePath from '@/components/organisms/monaco/utils/formatFilePath';
-import CodeEditor from '@/components/organisms/monaco/CodeEditor';
-import { ParsedInformation } from '@/types';
-import Card from '@/components/atoms/card';
-import { Chain, chains, getExplorerAddressUri } from '@/lib/chains';
-import Link from 'next/link';
-import Button from '@/components/atoms/button';
-import CreateBountyModal from '@/components/molecules/createBountyModal';
-import CreateBugModal from '@/components/molecules/createBugModal';
-import { useMemo } from 'react';
-import { BigNumber } from 'ethers';
-import { formatEther, formatUnits } from 'viem';
+import { useQuery } from "@tanstack/react-query";
+import toast from "react-hot-toast";
+import axios from "axios";
+import { usePathname } from "next/navigation";
+import { baseUrl } from "@/utils/constants";
+import formatFilePath from "@/components/organisms/monaco/utils/formatFilePath";
+import CodeEditor from "@/components/organisms/monaco/CodeEditor";
+import { ParsedInformation } from "@/types";
+import Card from "@/components/atoms/card";
+import { Chain, chains, getExplorerAddressUri } from "@/lib/chains";
+import Link from "next/link";
+import Button from "@/components/atoms/button";
+import CreateBountyModal from "@/components/molecules/createBountyModal";
+import CreateBugModal from "@/components/molecules/createBugModal";
+import { useMemo } from "react";
+import { BigNumber } from "ethers";
+import { formatEther, formatUnits } from "viem";
 
 export default function Explorer() {
   const pathname = usePathname();
-  const chain = pathname.split('/')[2];
-  const address = pathname.split('/')[3];
+  const chain = pathname.split("/")[2];
+  const address = pathname.split("/")[3];
 
   function getEditorData(contractInfo: any[] | undefined) {
     if (!contractInfo) {
@@ -38,7 +38,7 @@ export default function Explorer() {
   }
 
   const { data: editorData, isLoading: isLoadingContract } = useQuery({
-    queryKey: ['contract', chain, address],
+    queryKey: ["contract", chain, address],
     queryFn: async () => {
       try {
         const url = `${baseUrl}/api/contract/${chain}/${address}`;
@@ -47,7 +47,7 @@ export default function Explorer() {
 
         return editorData;
       } catch (e) {
-        toast.error('Something went wrong fetching contract information.');
+        toast.error("Something went wrong fetching contract information.");
         console.error(e);
         return null;
       }
@@ -87,12 +87,12 @@ export default function Explorer() {
   // });
 
   const { data: attestations } = useQuery({
-    queryKey: ['contract', 'status', chain, address],
+    queryKey: ["contract", "status", chain, address],
     queryFn: async () => {
       const loadingToastId = toast.loading(
-        'Fetching parsed data information...',
+        "Fetching parsed data information...",
         {
-          id: 'parsedData',
+          id: "parsedData",
         }
       );
 
@@ -100,12 +100,12 @@ export default function Explorer() {
         const url = `${baseUrl}/api/contract/index/${chain}/${address}`;
         const { data: attestations } = await axios.get(url, {});
 
-        toast.success('Successfully fethced parsed function data!', {
+        toast.success("Successfully fethced parsed function data!", {
           id: loadingToastId,
         });
         return attestations?.data || {};
       } catch (e) {
-        toast.error('Something went wrong fetching parsed data information.', {
+        toast.error("Something went wrong fetching parsed data information.", {
           id: loadingToastId,
         });
         console.error(e);
@@ -123,8 +123,8 @@ export default function Explorer() {
 
     return {
       totalBountyAmount: formatUnits(totalBountyAmount, 18),
-      totalBugsReported: attestations.reports.length - 1,
-      totalPayouts: attestations.reports.reduce((acc, report) => {
+      totalBugsReported: attestations.reports.length,
+      totalPayouts: attestations.reports.reduce((acc: any, report: any) => {
         if (report.status && report.status.amount > 0) {
           return acc + 1;
         }
@@ -148,7 +148,7 @@ export default function Explorer() {
 
   return (
     <div className="w-[1200px] pt-32 pb-12 flex flex-col gap-8">
-      <Card className={'bg-sidebar-background flex items-center'}>
+      <Card className={"bg-sidebar-background flex items-center"}>
         <div className="flex flex-col gap-2  w-[25%]">
           <h2 className="text-sm text-white">Chain</h2>
           <p className="text-white text-lg">
@@ -160,7 +160,7 @@ export default function Explorer() {
           <h2 className="text-sm text-white">Address</h2>
           <p className="text-white">
             <Link
-              target={'_blank'}
+              target={"_blank"}
               href={getExplorerAddressUri(Number(chain), address)}
               className="text-blue-300 text-xl"
             >
@@ -192,13 +192,13 @@ export default function Explorer() {
       </Card>
 
       <div className="flex gap-6">
-        <Card className={'bg-sidebar-background flex gap-6'}>
+        <Card className={"bg-sidebar-background flex gap-6"}>
           <div className="flex flex-col gap-2">
             <h2 className="text-xl text-white mb-2">Bounties</h2>
 
             {attestations?.length > 0 ? (
               <p className="text-white">
-                {attestations[0].schema.description || ''}
+                {attestations[0].schema.description || ""}
               </p>
             ) : (
               <>
@@ -207,7 +207,7 @@ export default function Explorer() {
                   className="py-0"
                   onClick={() => {
                     const modal: any = document.getElementById(
-                      'create-bounty-modal'
+                      "create-bounty-modal"
                     );
                     modal?.showModal() as any;
                   }}
@@ -219,7 +219,7 @@ export default function Explorer() {
           </div>
         </Card>
 
-        <Card className={'bg-sidebar-background flex gap-6'}>
+        <Card className={"bg-sidebar-background flex gap-6"}>
           <div className="flex flex-col gap-2">
             <h2 className="text-xl text-white mb-2">Bug reports</h2>
             <div className="text-white flex justify-center flex-col gap-3 w-full">
@@ -228,7 +228,7 @@ export default function Explorer() {
                 <Button
                   onClick={() => {
                     const modal: any =
-                      document.getElementById('create-bug-modal');
+                      document.getElementById("create-bug-modal");
                     modal?.showModal() as any;
                   }}
                 >
@@ -259,7 +259,11 @@ export default function Explorer() {
       )}
 
       <CreateBountyModal contractAddress={address} chainId={chain} />
-      <CreateBugModal contractAddress={address} chainId={chain} />
+      <CreateBugModal
+        contractAddress={address}
+        chainId={chain}
+        bountyId={attestations?.bounty[0]?.attestationId}
+      />
     </div>
   );
 }
